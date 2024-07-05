2e0e80a680239f87ec6053889c386c7036fa06fe
Ronny Cordova <rcordova@webcreek.com>
2024-07-05 12:57:34 -0500

    added missing mapper configs

---
+++ SimpleBankApp/SimpleBankApp.Api/Common/Mappings/BankAccountMappingConfig.cs
@@ -0,0 +1,27 @@
+﻿using Mapster;
+using SimpleBankApp.Api.Contracts.BankAccount.CreateBankAccount;
+using SimpleBankApp.Api.Contracts.BankAccount.DeleteBankAccount;
+using SimpleBankApp.Api.Contracts.BankAccount.DepositInBankAccount;
+using SimpleBankApp.Api.Contracts.BankAccount.GetBankAccount;
+using SimpleBankApp.Api.Contracts.BankAccount.WithdrawFromBankAccount;
+using SimpleBankApp.Application.BankAccount.Commands.CreateBankAccount;
+using SimpleBankApp.Application.BankAccount.Commands.DeleteBankAccount;
+using SimpleBankApp.Application.BankAccount.Commands.DepositInBankAccount;
+using SimpleBankApp.Application.BankAccount.Commands.WithdrawFromBankAccount;
+using SimpleBankApp.Application.BankAccount.Queries.GetBankAccount;
+
+namespace SimpleBankApp.Api.Common.Mappings
+{
+    public class BankMappingConfig : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            config.NewConfig<CreateBankAccountCommandResponse, CreateBankAccountResponse>()
+                .Map(dest => dest.AccountId, src => src.Id);
+            config.NewConfig<GetBankAccountCommandResponse, GetBankAccountResponse>();
+            config.NewConfig<DepositInBankAccountCommandResponse, DepositInBankAccountResponse>();
+            config.NewConfig<WithdrawFromBankAccountCommandResponse, WithdrawFromBankAccountResponse>();
+            config.NewConfig<DeleteBankAccountCommandResponse, DeleteBankAccountResponse>();
+        }
+    }
+}
--- SimpleBankApp/SimpleBankApp.Application/Common/Mappings/BankAccountMappingConfig.cs
+++ SimpleBankApp/SimpleBankApp.Application/Common/Mappings/BankAccountMappingConfig.cs
@@ -2,6 +2,7 @@
 using SimpleBankApp.Application.BankAccount.Commands.CreateBankAccount;
 using SimpleBankApp.Application.BankAccount.Commands.DepositInBankAccount;
 using SimpleBankApp.Application.BankAccount.Commands.WithdrawFromBankAccount;
+using SimpleBankApp.Application.BankAccount.Queries.GetBankAccount;
 using SimpleBankApp.Domain.Entities;
 
 namespace SimpleBankApp.Application.Common.Mappings
@@ -14,6 +15,8 @@ namespace SimpleBankApp.Application.Common.Mappings
             config.NewConfig<BankAccountEntity, DepositInBankAccountCommandResponse>()
                 .Map(dest => dest.AccountId, src => src.Id);
             config.NewConfig<BankAccountEntity, WithdrawFromBankAccountCommandResponse>()
+                .Map(dest => dest.AccountId, src => src.Id);
+            config.NewConfig<BankAccountEntity, GetBankAccountCommandResponse>()
                 .Map(dest => dest.AccountId, src => src.Id);
         }
     }
